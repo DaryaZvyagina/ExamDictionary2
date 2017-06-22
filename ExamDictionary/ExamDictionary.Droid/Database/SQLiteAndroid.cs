70068642b3f5598117888f018dc7de144fd1ec29
dima <otstavnov@ya.ru>
2017-06-22 12:02:15 +0300

    add sql lite model + sql-lite dependebcy

---
+++ ExamDictionary/ExamDictionary.Droid/Database/SQLiteAndroid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using ExamDictionary.Database;
+using ExamDictionary.Droid.Database;
+
+[assembly: Dependency(typeof(SQLiteAndroid))]
+
+namespace ExamDictionary.Droid.Database
+{
+    public class SQLiteAndroid : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "DataSQLite.db3";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsPath, sqliteFilename);
+            var conn = new SQLiteConnection(path);
+            return conn;
+        }
+    }
+}
---
+++ ExamDictionary/ExamDictionary/Models/History.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamDictionary.Models
+{
+    class History
+    {
+        string Word { get; set; }
+        string Translate { get; set; }
+    }
+}
